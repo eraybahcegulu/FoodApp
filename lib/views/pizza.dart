3c6158253867d799fb1b475a7140f9ac41c2ec1f
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-09-09 20:38:05 +0300

    detailed and pizzas added

--- lib/views/pizza.dart
+++ lib/views/pizza.dart
@@ -51,7 +51,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     PizzaData(
       color: Color.fromARGB(255, 82, 120, 143),
       recipeButtonColor: Color.fromARGB(255, 90, 211, 150),
-      image: AssetImage('assets/images/pizzas/margarita.png'),
+      image: AssetImage('assets/images/pizzas/margarita_pizza.png'),
       title: 'MARGARİTA PİZZA',
       text:
           'Malzemeler:\n• 1 paket (yaklaşık 7 gram) instant maya\n• 1 su bardağı ılık su\n• 2.5 su bardağı un\n• 2 yemek kaşığı sıvı yağ\n• 1 çay kaşığı tuz ve karabiber\n• 1/2 su bardağı domates sosu\n• 1 diş sarımsak (isteğe bağlı, rendelenmiş)\n• 1 çay kaşığı kurutulmuş kekik\n• 1.5 su bardağı rendelenmiş mozzarella peyniri\n• Taze fesleğen yaprakları (isteğe bağlı)',
@@ -63,7 +63,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     PizzaData(
       color: Color.fromRGBO(219, 121, 121, 1),
       recipeButtonColor: Color.fromARGB(255, 201, 50, 193),
-      image: AssetImage('assets/images/pizzas/havaii.png'),
+      image: AssetImage('assets/images/pizzas/havaii_pizza.png'),
       title: 'HAVAİİ PİZZA',
       text:
           'Malzemeler:\n• 1 paket (yaklaşık 7 gram) instant maya\n• 1 su bardağı ılık su\n• 2.5 su bardağı un\n• 2 yemek kaşığı sıvı yağ\n• 1 çay kaşığı tuz\n• 1/2 su bardağı domates sosu\n• 1/4 su bardağı barbekü sos\n• 1/4 su bardağı rendelenmiş mozzarella peyniri\n• 150 gram dilimlenmiş jambon veya hindi jambonu\n• 1/2 su bardağı döner parçaları (isteğe bağlı)\n• 1/2 su bardağı dilimlenmiş ananas (konserve veya taze)\n• 1/4 su bardağı dilimlenmiş siyah zeytin (isteğe bağlı)\n• 1/4 su bardağı dilimlenmiş yeşil biber (isteğe bağlı)\n• 1/4 su bardağı dilimlenmiş turşu biber (isteğe bağlı)',
@@ -76,7 +76,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
         PizzaData(
       color: Color.fromARGB(255, 129, 184, 67),
       recipeButtonColor: Color.fromARGB(255, 120, 154, 192),
-      image: AssetImage('assets/images/pizzas/pepperoni.png'),
+      image: AssetImage('assets/images/pizzas/pepperoni_pizza.png'),
       title: 'PEPPERONİ PİZZA',
       text:
           'Malzemeler:\n• 1 paket (yaklaşık 7 gram) instant maya\n• 1 su bardağı ılık su\n• 2.5 su bardağı un\n• 2 yemek kaşığı sıvı yağ\n• 1 çay kaşığı tuz\n• 1 su bardağı domates sosu\n• 1 diş sarımsak (isteğe bağlı, rendelenmiş)\n• 1 çay kaşığı kurutulmuş kekik\n• 1 çay kaşığı kurutulmuş fesleğen\n• Tuz ve karabiber (damak zevkinize göre)\n• 1.5 su bardağı rendelenmiş mozzarella peyniri\n• 100-150 gram dilimlenmiş pepperoni sosis\n•1/2 su bardağı dilimlenmiş siyah zeytin (isteğe bağlı)\n• 1/2 su bardağı dilimlenmiş yeşil biber (isteğe bağlı)\n• 1/4 su bardağı dilimlenmiş turşu biber (isteğe bağlı)',
--- lib/views/salata.dart
+++ lib/views/salata.dart
@@ -55,7 +55,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     SalataData(
       color: Colors.red,
       recipeButtonColor: const Color.fromARGB(255, 143, 54, 244),
-      image: AssetImage('assets/images/salads/akdeniz.png'),
+      image: AssetImage('assets/images/salads/akdeniz_salatasi.png'),
       title: 'AKDENİZ SALATASI',
       text:
           'Malzemeler:\n• 2 büyük domates\n• 1 salatalık\n• 1 yeşil biber\n• 1 kırmızı biber\n• 1 soğan\n• 1/2 su bardağı siyah zeytin (çekirdekleri çıkarılmış)\n• 1/2 su bardağı yeşil zeytin (çekirdekleri çıkarılmış)\n• 200 gram beyaz peynir veya lor peyniri\n• Taze roka veya taze nane yaprakları (isteğe bağlı)\n• 1/4 su bardağı zeytinyağı\n• 2 yemek kaşığı limon suyu\n• 1 diş sarımsak (isteğe bağlı, rendelenmiş\n• Tuz ve karabiber (damak zevkinize göre))',
@@ -67,7 +67,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     SalataData(
       color: const Color.fromARGB(255, 61, 204, 137),
       recipeButtonColor: Colors.red,
-      image: AssetImage('assets/images/salads/patates.png'),
+      image: AssetImage('assets/images/salads/patates_salatasi.png'),
       title: 'PATATES SALATASI',
       text:
           'Malzemeler:\n• 4 büyük patates\n• 3 yumurta (haşlanmış ve doğranmış)\n• 1/2 su bardağı mayonez\n• 2 yemek kaşığı yoğurt (isteğe bağlı)\n• 1 yemek kaşığı hardal (isteğe bağlı)\n• 2 çay kaşığı sirke veya limon suyu\n• 1/2 çay kaşığı tuz (veya damak zevkinize göre)\n• 1/4 çay kaşığı karabiber\n• 1/4 su bardağı doğranmış taze soğan (isteğe bağlı)\n •1/4 su bardağı doğranmış taze dereotu veya maydanoz (isteğe bağlı)',
@@ -79,7 +79,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     SalataData(
       color: const Color.fromARGB(255, 178, 77, 196),
       recipeButtonColor: Color.fromARGB(255, 37, 187, 94),
-      image: AssetImage('assets/images/salads/rus.png'),
+      image: AssetImage('assets/images/salads/rus_salatasi.png'),
       title: 'RUS SALATASI',
       text:
           'Malzemeler:\n• 4 büyük haşlanmış patates\n• 3 büyük haşlanmış havuç\n• 200 gram haşlanmış yeşil bezelye (konserve veya taze)\n• 200 gram haşlanmış döner parçaları (isteğe bağlı, tavuk veya hindi de kullanabilirsiniz)\n• 4 haşlanmış yumurta\n• 1/2 su bardağı haşlanmış mısır (konserve veya taze\n• 1/2 su bardağı mayonez\n• 1/4 su bardağı yoğurt (isteğe bağlı)\n• 2-3 yemek kaşığı hardal (isteğe bağlı)\n• Tuz ve karabiber (damak zevkinize göre)\n• 1/2 su bardağı doğranmış taze soğan (isteğe bağlı)\n• Taze dereotu veya maydanoz yaprakları (süslemek için)',
@@ -91,7 +91,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     SalataData(
       color: Colors.blue,
       recipeButtonColor: Color.fromARGB(255, 180, 65, 12),
-      image: AssetImage('assets/images/salads/sezar.png'),
+      image: AssetImage('assets/images/salads/sezar_salatasi.png'),
       title: 'SEZAR SALATASI',
       text:
           'Malzemeler:\n• 2 adet kıvırcık marul\n• 1/2 su bardağı rendelenmiş parmesan peyniri\n• 1 su bardağı çıtır ekmek kırıntıları veya krutonlar (isteğe bağlı)\n• 2 büyük tavuk göğsü\n• 2 yemek kaşığı zeytinyağı\n• Tuz ve karabiber\n• 1/2 su bardağı mayonez\n• 1/4 su bardağı rendelenmiş parmesan peyniri\n• 2 diş sarımsak (isteğe bağlı, rendelenmiş veya dövülmüş)\n• 2 yemek kaşığı limon suyu\n• 1 yemek kaşığı dijon hardalı\n• 1 yemek kaşığı Worcestershire sosu',
@@ -104,7 +104,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
     SalataData(
       color: Color.fromARGB(255, 173, 87, 120),
       recipeButtonColor: Color.fromARGB(255, 109, 126, 182),
-      image: AssetImage('assets/images/salads/makarna.png'),
+      image: AssetImage('assets/images/salads/makarna_salatasi.png'),
       title: 'MAKARNA SALATASI',
       text:
           'Malzemeler:\n• 2 su bardağı makarna (istediğiniz türde)\n• 1 su bardağı haşlanmış mısır (konserve veya taze)\n• 1/2 su bardağı doğranmış salatalık\n• 1/2 su bardağı doğranmış domates\n• 1/4 su bardağı doğranmış yeşil biber\n• 1/4 su bardağı doğranmış kırmızı biber\n• 1/4 su bardağı doğranmış mor soğan (isteğe bağlı)\n• 1/4 su bardağı doğranmış taze maydanoz veya taze dereotu (isteğe bağlı)\n• 1/2 su bardağı mayonez\n• 2 yemek kaşığı yoğurt (isteğe bağlı)\n• 2 yemek kaşığı sızma zeytinyağı\n• 2 yemek kaşığı beyaz sirke veya limon suyu\n• 1 diş sarımsak (isteğe bağlı, rendelenmiş veya ezilmiş\n• Tuz ve karabiber (damak zevkinize göre))',
@@ -117,7 +117,7 @@ class _ContainerChangerState extends State<ContainerChanger> {
         SalataData(
       color: Color.fromARGB(255, 57, 173, 102),
       recipeButtonColor: Color.fromARGB(255, 93, 119, 231),
-      image: AssetImage('assets/images/salads/kereviz.png'),
+      image: AssetImage('assets/images/salads/kereviz_salatasi.png'),
       title: 'KEREVİZ SALATASI',
       text:
           'Malzemeler:\n• 3 büyük kereviz (kabuklarını soyun ve doğrayın)\n• 1/2 su bardağı mayonez\n• 1 yemek kaşığı yoğurt (isteğe bağlı)\n• 1 yemek kaşığı limon suyu\n• 1 çay bardağı ceviz içi (isteğe bağlı, iri doğranmış)\n• 1/2 su bardağı ceviz içi (kabuklarından arındırılmış ve iri doğranmış)\n• Tuz ve karabiber (damak zevkinize göre)\n• Taze dereotu veya maydanoz yaprakları (süslemek için)',
